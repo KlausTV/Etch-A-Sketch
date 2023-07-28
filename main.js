6eec364c4c92877328d06ac1f6ee3474e3761c97
KlasterTV <elasticgaming10@gmail.com>
2023-07-27 21:38:36 -0400

    Still not done it does not work as of yet will be fixed soon

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Script executed");
+// Variables
+const container = document.querySelector(".container");
+
+function makeRows(numRows) {
+  // Calculate the total number of cells needed
+  const totalCells = numRows * numRows;
+
+  // Set the CSS style for the container to create a grid layout
+  container.style.gridTemplateColumns = `repeat(${numRows}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+
+  // Create and append cells to the container
+  for (let i = 0; i < totalCells; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+}
+
+makeRows(16);
